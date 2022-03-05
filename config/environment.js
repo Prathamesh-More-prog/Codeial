ae965e920728566e7221a7d9cbe4bbc36622b75f
Prathamesh-More-prog <prm.pm76@gmail.com>
2022-03-05 19:03:18 +0530

    codeial project

---
+++ config/environment.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const path = require('path');
+
+const logDirectory = path.join(__dirname, '../production_logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs('access.log', {
+    interval: '1d',
+    path: logDirectory
+});
+
+
+
+
+
+
+
+
+
+
+const development = {
+    name: 'development',
+    asset_path: './assets',
+    session_cookie_key: 'blahsomething',
+    db: 'codeial_development',
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user:'prathmeshmore795@gmail.com',
+            pass:'codingninjash123@'
+
+
+            }        },
+      google_client_id:"23705352305-g6rts04c9tf3ja4vrhj1thmijsscluue.apps.googleusercontent.com",
+      google_client_secret:"http://localhost:8000/users/auth/google/callback",
+
+      google_call_back_url:"http://localhost:8000/users/auth/google/callback",
+      jwt_secret: 'codeial',
+
+      morgan: {
+        mode: 'dev',
+        options: {stream: accessLogStream}
+    }
+
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+const production = {
+    name: 'production',
+    asset_path: process.env.CODEIAL_ASSET_PATH,
+    session_cookie_key: process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODEIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user:process.env.CODEIAL_GMAIL_USERNAME,
+            pass:process.env.CODEIAL_GMAIL_PASSWORD
+
+
+            }        },
+      google_client_id:process.env.CODEIAL_GOOGLE_CLIENT_ID,
+      google_client_secret:process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+
+      google_call_back_url: process.env.CODEIAL_GOOGLE_CALL_BACK_URL,
+      jwt_secret: process.env.CODEIAL_JWT_SECRET,
+
+      morgan: {
+        mode: 'combined',
+        options: {stream: accessLogStream}
+    }
+
+}
+
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT)==undefined ? development :eval(process.env.CODEIAL_ENVIRONMENT)
---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const gulp = require('gulp');
+
+// import dartSass from 'sass';
+// import nodeSass from "node-sass";
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+const cssnano = require('gulp-cssnano');
+const rev = require('gulp-rev');
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+
+
+//css
+
+gulp.task('css', function(done){
+    console.log('minifying css...');
+    gulp.src('./assets/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(gulp.dest('./assets.css'));
+
+     gulp.src('./assets/**/*.css')
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
+
+
+
+//js file
+
+gulp.task('js', function(done){
+    console.log('minifying js...');
+     gulp.src('./assets/**/*.js')
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done()
+});
+
+//imagesh file
+gulp.task('images', function(done){
+    console.log('compressing images...');
+    gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+    .pipe(imagemin())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
+
+// empty the public/assets directory
+gulp.task('clean:assets', function(done){
+    del.sync('./public/assets');
+    done();
+});
+
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
+    console.log('Building assets');
+    done();
+});
